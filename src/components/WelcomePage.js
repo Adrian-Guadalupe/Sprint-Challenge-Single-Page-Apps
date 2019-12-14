1dba53bdc0890110fdb7d7cd996e4a2c7e3a1f31
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-12-14 12:47:25 -0500

    added links

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,18 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
+      <header className='welcome-header'>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <Link to='/characters'>List of Characters</Link>
+      </header>
+      <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-      </header>
     </section>
   );
 }
