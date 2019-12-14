49435f5911257768aa7bdb20a9754a922d5de745
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-12-14 12:25:30 -0500

    CharacterList file done

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import  CharacterCard  from './CharacterCard';
+import SearchForm from './SearchForm';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [charArr, setCharArr] = useState([]);
+  // const [episodeArr, setEpisodeArr] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log(res.data.results);
+        setCharArr(res.data.results);
+      })
+      .catch(error => console.log(error))
   }, []);
 
+
+  /////////////////////////////////////////////////
+  // useEffect (() => {
+  //   charArr.map(char => {
+  //     console.log(char.episode);
+  //     char.episode.map(episode => {
+  //       axios
+  //         .get(episode)
+  //         .then(res => {
+  //           console.log(res.data);
+  //           console.log(res.data.name);
+  //           setEpisodeArr(res.data.name);
+  //         })
+  //     })
+  //   })
+  // })
+  //////////////////////////////////////////////////
+
+  
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <div className='navi'>
+        <Link to='/'>Home</Link>
+        <SearchForm characters={charArr}/>
+      </div>
+      <div className='card-container'>
+        {charArr.map(charObj => {
+          return <CharacterCard character={charObj} key={charObj.id} />
+        })}
+      </div>
     </section>
   );
 }
