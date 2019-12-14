6ac969c58a0385041a485c208a339197e86b949e
Adrian Guadalupe <safisweetkeyz@gmail.com>
2019-12-14 12:59:14 -0500

    search functionality done

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import CharacterCard from './CharacterCard';
 
-export default function SearchForm() {
- 
+export default function SearchForm({ characters }) {
+
+  const [searchChar, setSearchChar] = useState('');
+  const [searchResults, setSearchResults] = useState(characters);
+
+  useEffect(() => {
+    const results = characters.filter(character => {
+      return character.name.toLowerCase().includes(searchChar.toLowerCase());
+    })
+    setSearchResults(results);
+  }, [searchChar])
+
+  const handleChange = event => {
+    setSearchChar(event.target.value);
+  }
+
   return (
-    <section className="search-form">
-     // Add a search form here
+    <section>
+      <form className='search-form'> 
+        <label htmlFor='name'>Search Characters by Name:</label>
+        <input
+          id='name'
+          type='text'
+          name='textfield'
+          placeholder='  enter character name'
+          onChange={handleChange}
+          value={searchChar}
+        />
+      </form>
+
+      <div className='card-container'>
+        {searchResults.map(character => {
+          return <CharacterCard character={character} key={character.id}/>
+        })}      
+      </div>
     </section>
   );
 }
